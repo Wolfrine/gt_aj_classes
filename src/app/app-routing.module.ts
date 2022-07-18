01c9904f0249fa9271c57409d0d660f8cd7461eb
Wolfrine <schttewary@gmail.com>
2022-07-18 23:35:54 +0530

    Routes updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: ComponentsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
