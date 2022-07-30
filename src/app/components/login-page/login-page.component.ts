0d0e0667f6e1fcd179668897f43cc249b993dfd1
Wolfrine <schttewary@gmail.com>
2022-07-30 11:55:24 +0530

    Login page update, landing page corrections (#8)
    * Prod build
    
    * Login API call,Message displayed based  API resp
    
    * Setting up login page, landing page corrections

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,33 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { AppService } from '../../app.service';
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+    selector: 'app-login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
 
-  hide = true;
+    hide = true;
 
-  UserData: any;
-  message = "";
-  login = function (this: any) {
-    if (this.UserData != null) {
-      this.UserData = JSON.parse(this.UserData);
-      this.message = "Authentication successfull! Signing in ";
-      setTimeout(() => this.router.navigate(['/home']), 15000);
+    UserData: any;
+    message_body: any;
+
+    login: any;
+    check_auto_login: any;
+
+    constructor(private router: Router, private appservice: AppService) { }
+
+    ngOnInit(): void {
+
+        this.message_body = {
+            message: "",
+            status: "error"
+        };
+        var i = 1;
+        this.check_auto_login = setInterval(() => {
+            this.UserData = localStorage.getItem('UserData');
+            if (this.UserData) { clearInterval(this.check_auto_login); this.login(); }
+        }, 1000);
+
+
+        this.login = function () {
+            this.message_body.message = "Verifying"; this.message_body.status = "checking";
+
+            if (this.UserData) {
+                this.UserData = JSON.parse(this.UserData);
+
+                this.appservice.g_login(this.UserData).subscribe(x =>
+                    this.message_body = x
+                );
+                this.message_body = "Authentication successfull! Signing in ";
+            }
+        }
+
+
     }
-  }
-
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-    var i = 1;
-    setInterval(() => {
-      this.UserData = localStorage.getItem('UserData');
-      this.login();
-    }, 1000);
-  }
 
 }
